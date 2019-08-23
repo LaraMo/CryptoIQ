94544d3a0ed1a1cfd22594b2a28709067a8aeb72
nguyenquannnn <nguyenquan233@gmail.com>
2019-08-22 21:53:59 -0400

    #6 - Add more test cases Finish the class CipherWheel

--- src/server/src/puzzles/cipherwheel/index.js
+++ src/server/src/puzzles/cipherwheel/index.js
@@ -1,25 +1,25 @@
 class CipherWheel {
+    alphabet = []
     startCode = [];
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     innerWheel = [];
-    encodedMessage = ''
-    message = ''
+    encodedMessage = '';
+    message = '';
 
     constructor(message) {
+        this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         this.startCode = this.getRandomEncoder();
-        this.encodeMessage = this.encodeMessage(message);
+        this.encodedMessage = this.encodeMessage(message, this.alphabet, this.innerWheel);
         this.message = message;
-
     }
 
     getRandomEncoder() {
         let min = 0;
         let max = 25;
-        const pos1 = Math.floor(Number(Math.random()) * (max - min))  + min;
-        var pos2 = Math.floor(Number(Math.random()) * (max - min))  + min;
+        const pos1 = Math.floor(Number(Math.random()) * (max - min)) + min;
+        var pos2 = Math.floor(Number(Math.random()) * (max - min)) + min;
 
         while (pos1 === pos2) {
-            pos2 = Math.floor(Number(Math.random()) * (max - min))  + min;
+            pos2 = Math.floor(Number(Math.random()) * (max - min)) + min;
         }
 
         this.innerWheel = this.createInnerWheel(pos2 - pos1);
@@ -29,28 +29,44 @@ class CipherWheel {
     createInnerWheel(diff) {
         let result = [];
         if (diff > 0) {
-            let i = diff, j = 0;
-            while(i < this.alphabet.length || j < this.alphabet.length) {
-                let offset = i > this.alphabet.length  ? Math.abs(i - 25): i;
-                result[j] = this.alphabet[i];
+            let i = diff,
+                j = 0;
+            while (i < this.alphabet.length || j < this.alphabet.length) {
+                let offset = i >= this.alphabet.length ? Math.abs(i - 26) : i;
+                result[j] = this.alphabet[offset];
                 i++, j++;
             }
+        } else {
+            let i = diff + 26,
+                j = 0;
+            while (i < this.alphabet.length || j < this.alphabet.length) {
+                let offset = i >= this.alphabet.length ? Math.abs(i - 26) : i
+                result[j] = this.alphabet[offset];
+                i++, j++
+
+            }
         }
+        return result;
     }
 
-    encodeMessage() {
-        if (this.startCode.length !== 2) {
-            throw Error('Invalid start code!', this.startCode)
-        } else {
-            let result = '';
-            // this.message.forEach((val) => {
-            //     if (val === ' ') {
-            //         result += ' '
-            //     } else {
+    encodeMessage(message, outerWheel, innerWheel) {
+        let result = '';
+        message.split('').forEach((val) => {
+            if (val === ' ') {
+                result += ' ';
+            } else {
+                let letterIndex = innerWheel.findIndex(el => el.toUpperCase() === val.toUpperCase());
+                result += outerWheel[letterIndex];
+            }
+        })
+        return result;
+    }
 
-            //     }
-            // })
-        }
+    toString() {
+        console.log('message: ', this.message)
+        console.log('startCode: ', this.startCode);
+        console.log('innerWheel: ', this.innerWheel);
+        console.log('encodedMessage: ', this.encodedMessage)
     }
 }
 
--- src/server/src/puzzles/cipherwheel/index.test.js
+++ src/server/src/puzzles/cipherwheel/index.test.js
@@ -2,8 +2,41 @@ import CipherWheel from './index';
 
 test('generate encoder key', () => {
     const puzzle = new CipherWheel('hello');
-    console.log(puzzle.startCode)
+    console.log('Start code: ', puzzle.startCode)
     expect(puzzle.startCode.length).toBe(2);
     expect(puzzle.startCode[0]).toMatch(/[A-Z]/);
     expect(puzzle.startCode[1]).toMatch(/[A-Z]/);
+    expect(puzzle.startCode[1]).not.toStrictEqual(puzzle.startCode[0])
+})
+
+test('generate inner wheel forward direction', () => {
+    const puzzle = new CipherWheel('hello');
+    const wheel = puzzle.createInnerWheel(5);
+    const expectedOrder = ['F','G','H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','W','X','Y','Z','A','B','C','D','E']
+    expect(wheel.length).toBe(26);
+    expect(wheel).toStrictEqual(expectedOrder)
+})
+
+
+test('generate inner wheel backward direction', () => {
+    const puzzle = new CipherWheel('hello');
+    const wheel = puzzle.createInnerWheel(-3);
+    const expectedOrder = ['X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
+    expect(wheel.length).toBe(26);
+    expect(wheel).toStrictEqual(expectedOrder)
+})
+
+test('generate encoded message', () => {
+    const puzzle = new CipherWheel('hello');
+    const outerWheel = puzzle.alphabet;
+    const innerWheel = ['X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
+    const expected = 'KHOOR'
+    const actual = puzzle.encodeMessage('hello', outerWheel, innerWheel);
+    expect(actual).toStrictEqual(expected)
+})
+
+test('test class', () => {
+    const puzzle = new CipherWheel('bye');
+    console.log(puzzle);
+    expect('True').toBe('True');
 })
