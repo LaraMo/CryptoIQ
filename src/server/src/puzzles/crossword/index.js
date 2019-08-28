0264a616d8c2ea1940d7757266319a65a21ca11b
Nicholas Apanian <nicholas.apanian@outlook.com>
2019-08-27 20:01:11 -0400

    Continue work on implementing the crossword generator

--- src/server/src/puzzles/crossword/index.js
+++ src/server/src/puzzles/crossword/index.js
@@ -2,11 +2,90 @@ class Crossword {
     board = [];
     clues = [];
     wordsWithSimilarFirstLetters = []
-
+    
     constructor(clues) {
         this.clues = clues;
     }
     
+    get clues() {
+        return this.clues;
+    }
+    
+    get board() {
+        return this.board;
+    }
+
+    /**
+     * Initializes the game board with the dimensions of 100 x 100
+     * Once the board is full, it will be shrinked 
+     */
+    initBoard() {
+        for(let i = 0; i < 100; i ++) {
+            for(let j = 0; j < 100; j ++) {
+                this.board[i][j] = null;
+            }
+        }
+    }
+
+    /**
+     * Sorts the array of words by their length, largest to smallest
+     */
+    sortAnswersByLength() {
+        if(this.clues) {
+            this.clues.sort((a, b) => b.answer.length - a.answer.length)
+        }
+    }
+
+    /**
+     * Places all the words on the board
+     */
+    placeAnswersOnBoard() {
+        let startPosition = Math.floor(49 - (this.clues[0].answer.length/2)) ;        
+
+        this.clues.forEach((word, index) => {
+            if(index == 0) {
+                //place the first word in the middle of the board, and build the rest of the board around the first word
+                placeHorizontal(49, startPosition, word);
+            }
+            else {
+                //build the rest of the board
+             }
+
+        })
+    }
+
+    /**
+     * places the word horizontally on the board
+     * 
+     * @param {int} yPosition static y position
+     * @param {int} xStartPosition x start position, changes with every letter places
+     * @param {string} word the word to add to the board
+     */
+    placeHorizontal(yPosition, xStartPosition, word) {
+        Array.prototype.forEach.call(word, (letter, letterIndex) => {
+            this.board[yPosition][xStartPosition + letterIndex] = letter;
+        })
+    }
+
+    /**
+     * places the word vertically on the board
+     * 
+     * @param {int} xPosition static x position
+     * @param {int} yStartPosition y start position, changes with every letter placed
+     * @param {string} word the word to add to the board
+     */
+    placeVertical(xPosition, yStartPosition, word) {
+        Array.prototype.forEach.call(word, (letter, letterIndex) => {
+            this.board[yStartPosition + letterIndex][xPosition] = letter;
+        })
+    }
+
+
+
+
+
+
+
     /**
      * finds words which start with the same first letter and adds them to an array
      * from this array you can know which words can be used together (vertical + horizontal)
@@ -23,10 +102,6 @@ class Crossword {
         return this.wordsWithSimilarFirstLetters;
     }
 
-
-    get Crossword() {
-        return board;
-    }
 
     checkDuplicateArrayEntries(array, item) {
         for (let i = 0; i < array.length; i++) {
--- src/server/src/puzzles/crossword/index.test.js
+++ src/server/src/puzzles/crossword/index.test.js
@@ -19,4 +19,68 @@ test("Testing finding words with similar first letters", () => {
     const crossWord = new Crossword(data);
 
     expect(crossWord.findWordsWithSimilarFirstLetters()).toStrictEqual([["answer","axe"],["answer", "axium"], ["axe", "axium"]]);
-})
+});
+
+test("Testing Sorting the Answers by length, biggest to smallest", () => {
+    const data = [
+        {
+            answer: "a",
+        },
+        {
+            answer: "ab",
+        },
+        {
+            answer: "abc",
+        },
+        {
+            answer: "abcdef",
+        },
+        {
+            answer: "abcd",
+        },
+        {
+            answer: "abcdefg",
+        },
+        {
+            answer: "abcde",
+        },
+        {
+            answer: "aaa",
+        }
+    ]
+
+    const expected = [
+        {
+            answer: "abcdefg",
+        },
+        {
+            answer: "abcdef",
+        },
+        {
+            answer: "abcde",
+        },
+        {
+            answer: "abcd",
+        },
+        {
+            answer: "abc",
+        },
+        {
+            answer: "aaa",
+        },
+        {
+            answer: "ab",
+        },
+        {
+            answer: "a",
+        }
+    ]
+
+    
+
+    const crossWord = new Crossword(data);
+
+    crossWord.sortAnswersByLength();
+
+    expect(crossWord.clues).toStrictEqual(expected);
+});
