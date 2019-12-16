bcbffaab04d5e1b2af4522b657589ae10534a57e
Quan <nguyenquan233@gmail.com>
2019-12-16 02:00:23 -0500

    Update webpack prod

---
+++ src/client/webpack.prod.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: ['babel-polyfill', './index.js'],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+                test: /\.(jsx?)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader'
+                }]
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Production',
+            template: 'dist/index.html',
+            favicon: 'dist/favicon.ico'
+        })
+    ],
+};
